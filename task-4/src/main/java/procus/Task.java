87aae508991eb122d197f0b04ae85e1ab93c7e82
Zydiii <15826138027@163.com>
2019-05-26 20:28:15 +0800

    basic

---
+++ task-4/src/main/java/procus/Task.java
@@ -0,0 +1,27 @@
+package procus;
+
+public final class Task {
+    private String id;
+    private long time;
+
+    public Task(String id, long time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
